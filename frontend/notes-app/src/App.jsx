0bb0e1c3abb25bc8403b3921b9a57299f20779b2
Jahazel Sanchez <jahazelsanchez1@gmail.com>
2024-11-05 20:18:04 -0800

    update

---
+++ frontend/notes-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default App;
